d06985393b1558c6c0330697fa893f73d8bf7001
joakim engberg <joakim.engberg@gmail.com>
2017-12-02 16:33:34 +0100

    tog bort min mail

--- ip.sh
+++ ip.sh
@@ -5,5 +5,5 @@ oldip="`cat ip.txt`"
 
 if [ "$ip" != "$oldip" ]; then 
 `echo $ip > ip.txt`
-`mail -s "NYTT IP NUMMER!" "joakim.engberg@gmail.com" < ip.txt`
+`mail -s "NYTT IP NUMMER!" "mail@address.com" < ip.txt`
 fi
